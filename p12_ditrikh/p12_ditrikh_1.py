292a504e8255aa8ed613140af5ddeb75ea78b04e
Andriy Ditrikh <andriyditrikh@gmail.com>
2021-11-27 01:43:48 +0200

    p12

---
+++ p12_ditrikh/p12_ditrikh_1.py
@@ -0,0 +1,60 @@
+dirs = [
+    ( 'folder1',
+        [
+            'file1',
+            ( 'folder2', 
+                [
+                    'file2',
+                    'file3'
+                ] 
+            ),
+            ( 'folder3', 
+                [
+                    'file3', 
+                    'file4',
+                    ('folder4', ['file3'])
+                ] 
+            ),
+            'file5'
+        ]
+    )
+]
+
+# ВАШ КОД ТУТ
+
+def search(dirs, filename, way=[]):
+    if type(dirs) == tuple:
+        for i in dirs[1]:
+            if i == filename:
+                way = way + ["/" + dirs[0] + "/" + filename]
+            else:
+                way = way + ["".join(["/" + dirs[0] + "/"] + [x]) for x in search(i,filename)]
+        return way
+    else:
+        for i in dirs:
+            if type(i) == str:
+                if i == filename:
+                    return way + ["/" + filename]
+                else:
+                    return way
+            else:
+                return search(i, filename)
+
+# ПЕРЕВІРКА
+
+print(search(dirs, 'file1'))
+print(search(dirs, 'file2'))
+print(search(dirs, 'file3'))
+print(search(dirs, 'file4'))
+print(search(dirs, 'file5'))
+print(search(dirs, 'file6'))
+print(search(dirs, 'folder1'))
+
+assert search(dirs, 'file1') == ['/folder1/file1'], 'Failed test for file1'
+assert search(dirs, 'file2') == ['/folder1//folder2/file2'], 'Failed test for file2'
+assert search(dirs, 'file3') == ['/folder1//folder2/file3', '/folder1//folder3/file3', '/folder1//folder3//folder4/file3'], 'Failed test for file3'
+assert search(dirs, 'file4') == ['/folder1//folder3/file4'], 'Failed test for file4'
+assert search(dirs, 'file5') == ['/folder1/file5'], 'Failed test for file5'
+assert search(dirs, 'file6') == [], 'Failed test for file6'
+assert search(dirs, 'folder1') == [], 'Failed test for folder1'
+print('All tests good!')
